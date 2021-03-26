a2758e56101a27122ac4f2c72f60af71a61e2eea
MehrnazVahedian <elyseeway@gmail.com>
2021-03-26 09:53:29 -0700

    second commit !

--- app/src/main/java/vs/mehrnaz/hooshi/activity/MainActivity.java
+++ app/src/main/java/vs/mehrnaz/hooshi/activity/MainActivity.java
@@ -2,10 +2,19 @@ package vs.mehrnaz.hooshi.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.renderscript.ScriptGroup;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.PopupMenu;
 import android.widget.Toast;
 
 import com.github.mikephil.charting.data.PieData;
@@ -19,6 +28,7 @@ import java.util.List;
 
 import it.ngallazzi.fancyswitch.FancyState;
 import it.ngallazzi.fancyswitch.FancySwitch;
+import vs.mehrnaz.hooshi.LoginActivity;
 import vs.mehrnaz.hooshi.R;
 import vs.mehrnaz.hooshi.databinding.ActivityMainBinding;
 
@@ -30,6 +40,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding= DataBindingUtil.setContentView(this,R.layout.activity_main);
 
+        //chane Status Toolbar Background to gradient
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            Drawable background = getDrawable(R.drawable.gray_gradient_no_corner); //bg_gradient is your gradient.
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(getResources().getColor(android.R.color.transparent));
+            window.setBackgroundDrawable(background);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M)
+                window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
+        }
         FancySwitch fancySwitch = findViewById(R.id.fancySwitchButton);
         fancySwitch.setSwitchStateChangedListener(new FancySwitch.StateChangedListener() {
               @Override
@@ -62,6 +82,32 @@ public class MainActivity extends AppCompatActivity {
 
         binding.activityMainChart.setData(data);
 
+        binding.mainToolbar.toolbarSimpleMenuImageButton.setVisibility(View.VISIBLE);
+        binding.mainToolbar.toolbarSimpleMenuImageButton.setOnClickListener((View.OnClickListener) v -> {
+
+            PopupMenu popup = new PopupMenu(MainActivity.this, v);
+            popup.getMenuInflater().inflate(R.menu.pop_up, popup.getMenu());
+            popup.setOnMenuItemClickListener(item -> {
+                if (item.getItemId()==R.id.aboutItemMenu){
+                    startActivity(new Intent(MainActivity.this , AboutActivity.class));
+                }
+                else if (item.getItemId() == R.id.settingItemMenu){
+                    startActivity(new Intent(MainActivity.this , SettingActivity.class));
+                }
+                else if (item.getItemId() == R.id.logoutItemMenu){
+                  //  FirebaseAuth.getInstance().signOut();
+                    startActivity(new Intent(MainActivity.this , LoginActivity.class)
+                            .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK));
+                    finish();
+                }
+
+                return true;
+            });
+            popup.show();//showing popup menu
+        });
+
+        binding.mainToolbar.toolbarSimpleBackImageButton.setVisibility(View.GONE);
+        binding.mainToolbar.toolbarSimpleTitleTextView.setText(getString(R.string.app_name));
 
     }
 }
